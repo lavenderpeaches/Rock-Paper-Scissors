7dc4a478bb997452a960a344fd57aafd5423af51
Deepali <deepalix7@gmail.com>
2024-08-29 17:33:18 +0530

    Styled the page and made the buttons functional

--- script.js
+++ script.js
@@ -1,71 +1,73 @@
-// function getComputerChoice(){
-//     let randomNumber=Math.random();
+function getComputerChoice(){
+    let randomNumber=Math.random();
 
-//     let compChoice;
+    let compChoice;
 
-//     if (randomNumber>=0 && randomNumber<0.30){
-//         compChoice="rock";
-//     }
-//     else if (randomNumber>=0.30 && randomNumber<0.60){
-//         compChoice="paper"
-//     }
-//     else{
-//         compChoice="scissors"
-//     }
+    if (randomNumber>=0 && randomNumber<0.30){
+        compChoice="rock";
+    }
+    else if (randomNumber>=0.30 && randomNumber<0.60){
+        compChoice="paper"
+    }
+    else{
+        compChoice="scissors"
+    }
 
-//     return compChoice;
-// }
+    return compChoice;
+}
 
-// function getHumanChoice(){
-//     let humChoice=prompt("Make a choice: ");
-//     humChoice=humChoice.toLowerCase();
-//     return humChoice;
-// }
+function getHumanChoice(event){
+    let humChoice = event.target.innerText;
 
-// let humanScore=0, computerScore=0;
+    humChoice=humChoice.toLowerCase();
 
+    console.log(humChoice);
 
-// function playGame(){
+    return humChoice;
+}
 
-//     function playRound(humanChoice,computerChoice){
-//         let message;
-        
-//         if (humanChoice===computerChoice){
-//             message="Draw";
-//         }
-//         else if ((humanChoice==="rock" && computerChoice==="scissors") || (humanChoice==="paper" && computerChoice==="rock") || (humanChoice==="scissors" && computerChoice==="paper")){
-//             message=`You have won because ${humanChoice} beats ${computerChoice}`;
-//             humanScore++;
-//         }
-//         else{
-//             message=`You have lost because ${computerChoice} beats ${humanChoice}`;
-//             computerScore++;
-//         }
-        
-//        return message;
-//     }
+let humanScore=0, computerScore=0;
 
 
-//     for (let i = 0; i < 5; i++) {
-//         const humanSelection = getHumanChoice();
-//         const computerSelection = getComputerChoice();
-//         console.log(playRound(humanSelection, computerSelection));
-//     }
 
-//     final_result=`Computer Score: ${computerScore}\nHuman Score: ${humanScore}`
 
-//     console.log(final_result);
+function playRound(event){
+    const computerChoice = getComputerChoice();
+    const humanChoice = getHumanChoice(event);
 
-//    if(humanScore>computerScore){
-//     console.log("You have won! Congratulations.")
-//    }
-//    else if(computerScore>humanScore){
-//     console.log("Sorry, you have lost. Better luck next time")
-//    }
-//    else{
-//     console.log("It's a draw :D")
-//    }
-// }
+    console.log(computerChoice,humanChoice);
 
-// playGame()
+    let message;
+    
+    if (humanChoice===computerChoice){
+        message="Draw";
+    }
+    else if ((humanChoice==="rock" && computerChoice==="scissors") || (humanChoice==="paper" && computerChoice==="rock") || (humanChoice==="scissors" && computerChoice==="paper")){
+        message=`You have won because ${humanChoice} beats ${computerChoice}`;
+        humanScore++;
+    }
+    else{
+        message=`You have lost because ${computerChoice} beats ${humanChoice}`;
+        computerScore++;
+    }
+
+    document.querySelector("#Message").textContent = message;
+    document.querySelector("#HumanScore").textContent = `You : ${humanScore} `;
+    document.querySelector("#ComputerScore").textContent = `Computer : ${computerScore}`;
+
+    if (humanScore >= 5) {
+        document.querySelector("#Message").textContent = "Congratulations! You won the game!";
+       
+    } else if (computerScore >= 5) {
+        document.querySelector("#Message").textContent = "Sorry, you lost the game.";
+    
+    }
+
+}
+
+
+
+document.querySelector("#rock").addEventListener("click", playRound);
+document.querySelector("#paper").addEventListener("click", playRound);
+document.querySelector("#scissors").addEventListener("click", playRound);
 
